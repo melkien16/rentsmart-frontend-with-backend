2242437bffdd6ecddb64975c1b634eca5622e2c0
melkie yilk <melkieyilk@gmail.com>
2025-08-13 23:04:55 +0300

    add the disabled probs in the button

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -8,7 +8,7 @@ export const Button = ({
   icon: Icon,
   className = "",
   onClick,
-  disabled 
+  disabled ,
 }) => {
   const baseClasses =
     "inline-flex items-center justify-center font-bold rounded-xl transition-all duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-900";
