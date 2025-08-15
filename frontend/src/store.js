ca02f4be71f98fd9c68d179d9c9b403649524c5c
melkie yilk <melkieyilk@gmail.com>
2025-08-16 00:41:12 +0300

    add a booking slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
 import authSliceReducer from "./slices/authSlice";
 import walletSliceReducer from "./slices/walletSlice";
+import bookingSliceReducer from "./slices/bookingSlice"; // Import the booking slice
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSliceReducer,
     wallet: walletSliceReducer,
+    booking: bookingSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
