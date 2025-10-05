435e0ef1ee6a3abe36e75e40537d32bb847fdee6
melkie yilk <melkieyilk@gmail.com>
2025-10-05 17:09:24 +0300

    import and initiate some routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 
 import UserRouter from "./routes/user.routes.js";
 import ItemRouter from "./routes/item.routes.js";
@@ -13,8 +16,15 @@ import ReportRouter from "./routes/report.routes.js";
 import ReviewRouter from "./routes/review.routes.js";
 import CategoryRouter from "./routes/category.routes.js";
 import NotificationRouter from "./routes/notification.routes.js";
+import PublicSubscriberRouter from "./routes/publicSubscriber.routes.js";
+import SupportRouter from "./routes/support.routes.js";
 import UploadRouter from "./routes/upload.routes.js";
+import HelpRouter from "./routes/help.routes.js";
+import RobotChatRouter from "./routes/robotChar.routes.js";
+// import FaydaAuthRouter from "./routes/faydaAuth.routes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import path from "path";
+import chatSocket from "./socket.js";
 
 import connectDb from "./config/db.js";
 
@@ -42,6 +52,11 @@ app.use("/api/reviews", ReviewRouter);
 app.use("/api/categories", CategoryRouter);
 app.use("/api/notifications", NotificationRouter);
 app.use("/api/upload", UploadRouter);
+app.use("/api/support", SupportRouter);
+app.use("/api/help", HelpRouter);
+app.use("/api/robotchat", RobotChatRouter);
+app.use("/api/subscribe", PublicSubscriberRouter);
+// app.use("/api/fayda-auth", FaydaAuthRouter);
 
 // Production-specific configuration
 if (process.env.NODE_ENV === "production") {
@@ -70,8 +85,22 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
+const server = http.createServer(app);
+// Initialize socket.io
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+// Set up chat socket
+chatSocket(io);
+
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
