2d79c7c699933ca71e5bccaefba28af81107e199
melkie yilk <melkieyilk@gmail.com>
2025-10-06 15:50:56 +0300

    data seeded

--- backend/seeder.js
+++ backend/seeder.js
@@ -3,14 +3,25 @@ import dotenv from "dotenv";
 import colors from "colors";
 
 import users from "./data/users.js";
+import collaterals from "./data/collateral.js";
+import reports from "./data/report.js";
+import reviews from "./data/review.js";
+import subscribeEmails from "./data/subscribeEmails.js";
+import wallets from "./data/walets.js";
 import categories from "./data/category.js";
 import messages from "./data/message.js";
 import products from "./data/items.js";
+
 import Product from "./models/itemModel.js";
 import User from "./models/userModel.js";
 import Order from "./models/bookingModel.js";
 import Category from "./models/categoryModel.js";
 import Message from "./models/messageModel.js";
+import Report from "./models/reportModle.js";
+import Review from "./models/reviewModel.js";
+import SubscribeEmail from "./models/publicSubscribers.js";
+import Wallet from "./models/walletModel.js";
+import Collateral from "./models/collateralModel.js";
 import connectDb from "./config/db.js";
 
 dotenv.config();
@@ -23,6 +34,17 @@ const importData = async () => {
     await User.deleteMany();
     await Category.deleteMany();
     await Message.deleteMany();
+    await Report.deleteMany();
+    await Review.deleteMany();
+    await SubscribeEmail.deleteMany();
+    await Wallet.deleteMany();
+    await Collateral.deleteMany();
+
+    await Collateral.insertMany(collaterals);
+    await Report.insertMany(reports);
+    await Review.insertMany(reviews);
+    await SubscribeEmail.insertMany(subscribeEmails);
+    await Wallet.insertMany(wallets);
 
     const createdUsers = await User.insertMany(users);
     const adminUser = createdUsers[0]._id;
