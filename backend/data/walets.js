86603a78bfdaee5fe40d783631be59021b77dd73
melkie yilk <melkieyilk@gmail.com>
2025-10-06 15:53:56 +0300

    dummy data modification

--- backend/data/walets.js
+++ backend/data/walets.js
@@ -1,47 +1,53 @@
 const wallets = [
   {
-    user: '68126a5f169827da2ebdf609',
-    balance: 500,
-    transactions: [
-      {
-        type: "credit",
-        amount: 500,
-        description: "Initial deposit",
-      },
-    ],
+    _id:  "6873d2abf353fe308401b0bb",
+    user: "689aa5de797dfb5254ac4d16",
+    balance: 1873.3100000000002,
   },
   {
-    user: '68126a5f169827da2ebdf60a',
-    balance: 1200,
-    transactions: [
-      {
-        type: "credit",
-        amount: 1000,
-        description: "Referral bonus",
-      },
-      {
-        type: "credit",
-        amount: 200,
-        description: "Item rental income",
-      },
-    ],
+    _id:  "6873d2abf353fe308401b0bc",
+    user: "689aa5de797dfb5254ac4d17",
+    balance: 7669.6,
   },
   {
-    user: '68126a5f169827da2ebdf608',
-    balance: 150,
-    transactions: [
-      {
-        type: "credit",
-        amount: 200,
-        description: "Top-up",
-      },
-      {
-        type: "debit",
-        amount: 50,
-        description: "Item rental payment",
-      },
-    ],
+    _id:  "6873d2abf353fe308401b0bd",
+    user: "689aa5de797dfb5254ac4d18",
+    balance: 8525.34,
+  },
+  {
+    _id:  "6873d37d34d3f2ff7f03e934",
+    user: "6873d2abf353fe308401b0b4",
+    balance: 8752.51,
+  },
+  {
+    _id:  "689a9e0668325814f6e53a7b",
+    user: "689a9e0668325814f6e53a79",
+    balance: 0,
+  },
+  {
+    _id:  "689aa38068325814f6e53a80",
+    user: "689aa38068325814f6e53a7e",
+    balance: 0,
+  },
+  {
+    _id:  "689b1ddc624fdb2be4fc196e",
+    user: "689b1ddc624fdb2be4fc196c",
+    balance: 0,
+  },
+  {
+    _id:  "689c8bce9d1ce6df66633663",
+    user: "689c8bce9d1ce6df66633661",
+    balance: 0,
+  },
+  {
+    _id:  "68e2b436382b0568feaa8644",
+    user: "68e2b436382b0568feaa8642",
+    balance: 0,
+  },
+  {
+    _id: "68e2b5db382b0568feaa8688",
+    user: "68e2b5db382b0568feaa8686",
+    balance: 0,
   },
 ];
-
 export default wallets;
