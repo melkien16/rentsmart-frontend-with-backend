425eaad296717a82b8925f2da23b5e3c74a99ee6
melkie yilk <melkieyilk@gmail.com>
2025-10-06 21:56:18 +0300

    add some message api slice

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -6,13 +6,17 @@ import {
   getAllMessages,
   markMessageAsRead,
   deleteMessage,
+  getConversationById,
+  getUserMessageByEmail,
 } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.post("/", protect, sendMessage);
 router.get("/", protect, getAllMessages);
-router.get("/:userId", protect, getMessages);
+router.get("/user/:userId", protect, getMessages);
+router.get("/email/:email", protect, getUserMessageByEmail);
+router.get("/conversation/:conversationId", protect, getConversationById);
 router.put("/:id/read", protect, markMessageAsRead);
 router.delete("/:id", protect, deleteMessage);
 
